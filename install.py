53cb4ba5ca1f9d2dd36101315e73927483f134ef
vesche <austinjackson892@gmail.com>
2017-06-10 22:23:40 -0400

    Remove semi-colons from install.py

--- install.py
+++ install.py
@@ -8,7 +8,7 @@ import string
 import subprocess
 
 
-WARNINGS = 0;
+WARNINGS = 0
 
 
 class bcolors:
@@ -25,20 +25,20 @@ class bcolors:
 def Check_Root():
     if os.getuid() != 0:
         print(bcolors.FAIL+"[-] Must be run as root."+bcolors.ENDC)
-        sys.exit(0);
-    return 0;
+        sys.exit(0)
+    return 0
 
 
 def Find_Package_Manager():
-    Package_Managers = ["apt-get", "pacman", "yum"];
+    Package_Managers = ["apt-get", "pacman", "yum"]
 
     for manager in Package_Managers:
-        Default_Package_Manager = os.popen("which "+manager).read();
+        Default_Package_Manager = os.popen("which "+manager).read()
         if Default_Package_Manager not in range(0, 300):
-            return Default_Package_Manager.split("/")[-1].strip();
+            return Default_Package_Manager.split("/")[-1].strip()
 
-    print(bcolors.FAIL+"[-] No Default Package Manager Found"+bcolors.ENDC);
-    sys.exit(0);
+    print(bcolors.FAIL+"[-] No Default Package Manager Found"+bcolors.ENDC)
+    sys.exit(0)
 
 
 def Install_Packages(Package_Manager):
@@ -47,21 +47,21 @@ def Install_Packages(Package_Manager):
                             "libncap-dev", "iw", "tcpdump",
                             "graphviz", "python-gnuplot",
                             "python-crypto"
-                        ];
-    Failed_Packages = [];
+                        ]
+    Failed_Packages = []
 
     for package in Packages_To_Install:
         try:
-            subprocess.check_output([Package_Manager, "install", package, "-y"], stderr=subprocess.STDOUT);
+            subprocess.check_output([Package_Manager, "install", package, "-y"], stderr=subprocess.STDOUT)
             print(bcolors.OKGREEN+"[+] Installed: "+package+bcolors.ENDC)
         except subprocess.CalledProcessError as e:
-            Failed_Packages.append( package );
-            WARNINGS += 1;
+            Failed_Packages.append( package )
+            WARNINGS += 1
 
     if len(Failed_Packages) > 0:
         for item in Failed_Packages:
-            print(bcolors.WARNING+"[-] Failed to install Package: "+item+bcolors.ENDC);
-    return;
+            print(bcolors.WARNING+"[-] Failed to install Package: "+item+bcolors.ENDC)
+    return
 
 
 def Create_Custom_Command():
@@ -84,50 +84,50 @@ def Create_Custom_Command():
 
     for dire in dirs:
         if os.path.isdir(dire):
-            os.system("sudo rm -rf "+dire);
-            print(bcolors.OKGREEN+"[+] Removed Old Project Directory"+bcolors.ENDC);
+            os.system("sudo rm -rf "+dire)
+            print(bcolors.OKGREEN+"[+] Removed Old Project Directory"+bcolors.ENDC)
 
     try:
         subprocess.check_output(["sudo", "cp", "-r", "Packet-Sniffer/", "/usr/share/"], stderr=subprocess.STDOUT)
-        subprocess.check_output(["sudo", "cp", "-r", "Monitor/", "/usr/share/"], stderr=subprocess.STDOUT);
-        subprocess.check_output(["sudo", "cp", "-r", "Deauth/", "/usr/share/"], stderr=subprocess.STDOUT);
+        subprocess.check_output(["sudo", "cp", "-r", "Monitor/", "/usr/share/"], stderr=subprocess.STDOUT)
+        subprocess.check_output(["sudo", "cp", "-r", "Deauth/", "/usr/share/"], stderr=subprocess.STDOUT)
         print(bcolors.OKGREEN+"[+] Installed Tools to: /usr/share/"+bcolors.ENDC)
     except subprocess.CalledProcessError as e:
-        print(e.output);
+        print(e.output)
 
     for link in links:
         if os.path.islink(link):
-            os.system("sudo rm -f "+link);
-            print(bcolors.OKGREEN+"[+] Removed an old command"+bcolors.ENDC);
+            os.system("sudo rm -f "+link)
+            print(bcolors.OKGREEN+"[+] Removed an old command"+bcolors.ENDC)
 
     for index, value in enumerate(new_links):
         try:
-            subprocess.check_output(["sudo", "ln", "-s", new_links[index], links[index]], stderr=subprocess.STDOUT);
-            subprocess.check_output(["sudo", "chmod", "755", links[index]], stderr=subprocess.STDOUT);
+            subprocess.check_output(["sudo", "ln", "-s", new_links[index], links[index]], stderr=subprocess.STDOUT)
+            subprocess.check_output(["sudo", "chmod", "755", links[index]], stderr=subprocess.STDOUT)
             print(bcolors.OKGREEN+"[+] Created New Command"+bcolors.ENDC)
         except subprocess.CalledProcessError as e:
-            print(bcolors.FAIL+"[-] Failed During custom command creation.");
+            print(bcolors.FAIL+"[-] Failed During custom command creation.")
 
-    return 0;
+    return 0
 
 
 def install():
     global WARNINGS
 
-    Check_Root();
+    Check_Root()
 
     print(bcolors.OKBLUE+"""
 ,_,_,_,_,_,_,_,_,_,_|____________________BOOP-INSTALLER____________________
 |#|#|#|#|#|#|#|#|#|#|_____________________________________________________/
 '-'-'-'-'-'-'-'-'-'-|----------------------------------------------------'
                                           M1ND-B3ND3R
-    """+bcolors.ENDC);
+    """+bcolors.ENDC)
 
-    Package_Manager = Find_Package_Manager();
-    Install_Packages(Package_Manager);
-    Create_Custom_Command();
+    Package_Manager = Find_Package_Manager()
+    Install_Packages(Package_Manager)
+    Create_Custom_Command()
 
     print(bcolors.HEADER+"[+] Exiting with: "+str(WARNINGS)+" warnings and 0 Failures.")
 
 
-install();
+install()
